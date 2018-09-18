6047036f1de60eb63cd1917fc1df993cbb6f57dd
Adit <dummy@email.com>
2018-09-18 16:59:16 +0800

    Minor changes to UI

--- src/main/java/com/careertimeline/adit/controller/ProfileHomeController.java
+++ src/main/java/com/careertimeline/adit/controller/ProfileHomeController.java
@@ -1,4 +1,13 @@
 package com.careertimeline.adit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProfileHomeController {
+
+    @GetMapping("/profileHome")
+    public String greeting() {
+        return "ProfileHome";
+    }
 }
